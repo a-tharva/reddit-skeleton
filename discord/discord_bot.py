9c43d2c0a2d62218cc68eaf09cd9bf918d93813b
atharva bhandvalkar <70326109+a-tharva@users.noreply.github.com>
2022-01-04 20:30:36 +0530

    Update discord_bot.py

--- discord/discord_bot.py
+++ discord/discord_bot.py
@@ -1,1 +1,19 @@
-pass
+import discord
+from config import TOKEN
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as {0.user}'.format(client))
+    
+@client.event
+async def on_message(message):
+    if messge.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+client.run(TOKEN)
